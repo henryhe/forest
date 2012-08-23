d0d97237830866fc37c59de35c66de5b61c2879c
henryhe <changtianbonbon@gmail.com>
2012-08-23 10:49:21 +0800

    [code] add some note ,move some public function to locate.c
    添加一些注释，把一些函数移动到locate.c

--- src/locate.c
+++ src/locate.c
@@ -3,7 +3,7 @@
  *
  *       Filename:  locate.c
  *
- *    Description:  locate中的共用函数
+ *    Description:  locate中的公用函数
  *
  *        Version:  1.0
  *        Created:  2012年08月21日 18时12分53秒
@@ -22,4 +22,56 @@ void freeR(struct record *r)
     free(r->key);
     free(r->e);
     free(r);
+}
+
+/*
+ * function:根据两个指针之间的内容，生成新的字符串，新申请的空间，需要free
+ * input   :两个字符指针，start,end;start指向需要内容的第一个字符，end指向末尾字符的后边一个空间
+ * output  :返回新生成的字符串，malloc了空间，需要free
+ */
+char *createstr(char *start, char *end)
+{
+    int len = end - start ;
+    char *result  = (char *)malloc(len + 1);
+    memcpy(result, start, len);
+    *(result + len) = '\0';
+    return result;
+}
+
+/* 
+ * function：判断kye的类型
+ * args：char *key
+ * return:代表类型的字符，b:基站；w:wifi
+ */
+char getkeytype(char *key)
+{
+    char *p = key;
+    while (TRUE)
+    {
+        if (*p == '\0')
+            break;
+        if (*p == '.')
+            return 'b';
+        if (*p == ':')
+            return 'w';
+        p++;
+    }
+    return '0';
+}
+
+/* 
+ * function:截取一个字符串的子串，转化为int
+ * args:char *ori 原始字符串，int from 字串起始位置，int to 字串结束位置
+ * return:对应的整数
+ */
+int getint(char *ori, int from , int to)
+{	
+    int n = to - from + 1;
+	ori += from;
+	char* temp = (char*)malloc(n+1);
+	memcpy(temp, ori, n);
+	*(temp + n) = '\0';
+	int result = atoi(temp);
+	free(temp);
+	return result;
 }
--- src/parselog.c
+++ src/parselog.c
@@ -23,9 +23,11 @@
 static struct stat s;
 
 /**
-  * 返回日志来源，包含“&_x=”的日志判定为来源于定位，加上‘&’是因为cormorant日志里有些日志
-  * 包含“lu=”的即判定为cormorant行为日志来的信息
-  * 返回值为0，1，2，0为无效数据，1为tk的日志，2为cor的日志
+  * function:返回日志来源，包含“&_x=”的日志判定为来源于定位，
+  *          加上‘&’是因为cormorant日志里有些日志
+  *          包含“lu=”的即判定为cormorant行为日志来的信息
+  * input   :一个字符串指针
+  * retrun  :返回值为0，1，2，0为无效数据，1为tk的日志，2为cor的日志
   */
 int logtype(char *line) 
 {
@@ -42,59 +44,11 @@ int logtype(char *line)
 	}
 	return 0;
 }
-
 /*
- * 根据两个指针之间的内容，生成新的字符串，新申请的空间，需要free
- * 注：start指向需要内容的第一个字符，end指向末尾字符的后边一个空间
+ * function: 将格式如2012-07-15 00:00:00的字符串转化为1900年到该时间的秒数
+ * input   : 含有时间信息的字符串，格式如上
+ * return  : 代表秒数的long
  */
-char *createstr(char *start, char *end)
-{
-    int len = end - start ;
-    char *result  = (char *)malloc(len + 1);
-    memcpy(result, start, len);
-    *(result + len) = '\0';
-    return result;
-}
-
-/* 
- * function：判断kye的类型
- * args：char *key
- * return:代表类型的字符，b:基站；w:wifi
- * */
-char getkeytype(char *key)
-{
-    char *p = key;
-    while (TRUE)
-    {
-        if (*p == '\0')
-            break;
-        if (*p == '.')
-            return 'b';
-        if (*p == ':')
-            return 'w';
-        p++;
-    }
-    return '0';
-}
-
-/* 
- * function:截取一个字符串的子串，转化为int
- * args:char *ori 原始字符串，int from 字串起始位置，int to 字串结束位置
- * return:对应的整数
- * */
-int getint(char *ori, int from , int to)
-{	
-    int n = to - from + 1;
-	ori += from;
-	char* temp = (char*)malloc(n+1);
-	memcpy(temp, ori, n);
-	*(temp + n) = '\0';
-	int result = atoi(temp);
-	free(temp);
-	return result;
-}
-
-//将格式如2012-07-15 00:00:00的字符串转化为1900年到该时间的秒数
 long gettime( char* line )
 {
 	char *timeinfo = (char *)malloc(20);
@@ -113,6 +67,12 @@ long gettime( char* line )
 	return timep;
 }
 
+/* 
+ * function: 处理每条日志的参数，调用一次处理一个参数，放到log结构中
+ * input   : log结构指针，key，value指针，放入到log的list参数值
+ *           的key会被free掉，无需再处理
+ * return  ：无返回值，操作传入的log
+ * */
 void dealpara(struct log *log, char *key, char *value )
 {
 	if (strcmp(key, "wifi_mac[]\0") == 0)
@@ -485,11 +445,11 @@ void printne(struct list *list)
 
 void printR(struct record r){
     s.keynum++;
-    printf("%s ", r.key);
-    printf("%ld ", r.time);
-    printf("%lf %lf %lf ", r.x, r.y, r.p);
-    printf("%d ", r.type);
-    printf("%s\n", r.e);
+    //printf("%s ", r.key);
+    //printf("%ld ", r.time);
+    //printf("%lf %lf %lf ", r.x, r.y, r.p);
+    //printf("%d ", r.type);
+    //printf("%s\n", r.e);
 }
 
 //得到来自tk日志的结果
